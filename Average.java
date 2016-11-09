5652716fd529c2f37bddbfa8cf37af90bce02fac
oracle <oracle@GBTC441R06RH.montgomerycollege.edu>
2016-11-09 18:58:28 -0500

    first commit

---
+++ Average.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Average
+{ 
+  public static void main( String[] args ) 
+  { 
+    Scanner keyboard = new Scanner(System.in);
+
+    double num1, num2;
+
+    System.out.print( "First temperature? " );
+    num1 = keyboard.nextInt();
+
+    System.out.print( "Second temperature? " );
+    num2 = keyboard.nextInt();
+
+    System.out.println("The avg value is : " + (num1+num2)/2);
+    keyboard.close();
+  }
+}
